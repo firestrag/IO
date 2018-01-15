3d37bd3cd288dec797c1e1933241f2853a13bd27
kajkwi <kajkwi@poczta.onet.pl>
2018-01-15 14:26:11 +0100

    view
    add html view client login

--- app/application/view/Frontend/Index/login.php
+++ app/application/view/Frontend/Index/login.php
@@ -1,5 +1,45 @@
-<?php
 
-//var_dump($users_list);
 
-?>
+<!--Navigation-->
+<header style="height: 100%">
+
+    <!-- Intro Section -->
+    <div id="home" class="view jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('public/img/1.jpg');">
+        <div class="full-bg-img">
+            <div class="container flex-center">
+                <div class="row smooth-scroll">
+                    <div class="col-md-12 white-text text-center">
+                        <div class="wow fadeInDown" data-wow-delay="0.2s">
+                            <h2 class="display-1 font-bold font-up mb-2 spacing rgba-white-slight px-4 py-3">
+                                <strong>Ubezpieczenia turystyczne</strong>
+                            </h2>
+
+                            <h4 class="subtext-header mt-4 mb-5"><p>Ubezpieczenie turystyczne pozwala objąć ochroną zarówno podróż zagraniczną, jak i krajową.</p>
+                                <p>Szeroki zakres ochrony, możesz wybrać wariant dostosowany do swoich potrzeb.</p></h4>
+                        </div>
+                        <form>
+                            <div style="background-color: #ffffff">
+                                <select class="mdb-select" style="background-color: #ffffff">
+                                    <option value="" disabled selected>Wybierz swoje konto</option>
+                                    <option value="1">Klient 1</option>
+                                    <option value="2">Klient 2</option>
+                                    <option value="3">Klient 3</option>
+                                </select>
+                            </div>
+
+                            <button type="button" class="btn btn-success btn-rounded">Zaloguj</button>
+
+                        </form>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</header>
+
+
+
+
+
