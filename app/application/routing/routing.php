df42d277bb230af0bef8b56f5602a80e95b00aed
firestrag <firestrag@gmail.com>
2018-01-19 11:43:51 +0100

    change routing

--- app/application/routing/routing.php
+++ app/application/routing/routing.php
@@ -11,5 +11,23 @@ return [
         'url' => [],
         'controller' => 'Frontend_Index',
         'action' => 'login',
+    ],
+
+    'wyloguj' => [
+        'ulr' => [],
+        'controller' => 'Fronted_Index',
+        'action' => 'logout'
+    ],
+
+    'wszystkie-ubezpieczenia' => [
+        'ulr' => [],
+        'controller' => 'Fronted_Index',
+        'action' => 'insurancesAll'
+    ],
+
+    'twoje-ubezpieczenia' => [
+        'ulr' => [],
+        'controller' => 'Fronted_Index',
+        'action' => 'insurancesList'
     ]
 ];
