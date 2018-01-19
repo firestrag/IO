1a2b47d66e9e8314fff51583d790b7ade05a24db
kajkwi <kajkwi@poczta.onet.pl>
2018-01-19 12:54:15 +0100

    Jestem piękna ^^

--- app/application/class/Controller/Frontend/Index.php
+++ app/application/class/Controller/Frontend/Index.php
@@ -101,6 +101,41 @@ class Controller_Frontend_Index extends Controller{
             ->bind('user',$user);
     }
 
+    public function action_insurancesAll(){
 
+        $insurances = [
+            [
+                'id' => '1',
+                'name' => 'Wycieczka zagraniczna',
+                'description' => 'Wycieczka zagraniczna do arabskiego burdelu, gdzie czekają na ciebie chętne kozy',
+                'price_per_month' => '1920',
+                'date_start' => '2017-03-12',
+                'date_end' => '2017-04-11',
+            ],
+            [
+                'id' => '2',
+                'name' => 'Wycieczka w Polsce',
+                'description' => 'Obóz rekreacyjny',
+                'price_per_month' => '999',
+                'date_start' => '2017-03-12',
+                'date_end' => '2017-04-11',
+            ],
+            [
+                'id' => '3',
+                'name' => 'Ubezpieczenie rodzinne',
+                'description' => 'Ubezpieczenie całej rodziny na wakacyjny wyjazd',
+                'price_per_month' => '3200',
+                'date_start' => '2017-03-12',
+                'date_end' => '2017-04-11',
+            ],
+        ];
 
+        $this->template->view = Power_View::factory('Frontend/Index/insurancesAll')
+            ->bind('insurances', $insurances);
+    }
+
+    public function action_addInsurance(){
+//        Model_Insurances::add()
+        var_dump(Power_Params::getParam(1)); die();
+    }
 }
