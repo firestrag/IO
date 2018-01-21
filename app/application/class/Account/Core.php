0099ec55b2d955c04d54a564a2ccd8b97e73ad64
firestrag <firestrag@gmail.com>
2018-01-21 21:59:24 +0100

    add account type

--- app/application/class/Account/Core.php
+++ app/application/class/Account/Core.php
@@ -3,13 +3,22 @@
 abstract class Account_Core{
 
     protected static $_login_id = 'Account_Core__id';
+    protected static $_account_type = 'Account_Core__type';
 
-    public static function isLogin(){
-        return isset($_SESSION[self::$_login_id]);
+    public static function isLogin($type){
+        return (
+            isset($_SESSION[self::$_login_id]) &&
+            (isset($_SESSION[self::$_account_type])) &&
+            ($_SESSION[self::$_account_type] == $type)
+        );
     }
 
     protected static function setLoginId($id){
         $_SESSION[self::$_login_id] = $id;
+    }
+
+    protected static function setAccountType($type){
+        $_SESSION[self::$_account_type] = $type;
     }
 
     abstract function login();
